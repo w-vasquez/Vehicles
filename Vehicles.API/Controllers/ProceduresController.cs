7661a6164ec86975c769a078161ee277f151fd23
Wilfredo Vásquez <w_vasquez@outlook.com>
2021-09-04 13:32:22 -0600

    Added CRUD procedures

---
+++ Vehicles.API/Controllers/ProceduresController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+	public class ProceduresController : Controller
+	{
+		private readonly DataContext _context;
+		public ProceduresController(DataContext context)
+		{
+			_context = context;
+		}
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Procedures.ToListAsync());
+        }
+
+        // GET: VehicleTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Procedure procedure)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(procedure);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe el procedimiento.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(procedure);
+        }
+
+        // GET: VehicleTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Procedure procedure = await _context.Procedures.FindAsync(id);
+            if (procedure == null)
+            {
+                return NotFound();
+            }
+            return View(procedure);
+        }
+
+        // POST: VehicleTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Procedure procedure)
+        {
+            if (id != procedure.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(procedure);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe procedimiento.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(procedure);
+        }
+
+        // GET: VehicleTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Procedure procedure = await _context.Procedures
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (procedure == null)
+            {
+                return NotFound();
+            }
+
+            _context.Procedures.Remove(procedure);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Vehicles.API/Migrations/20210904185836_AddTableProceduresIndex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class AddTableProceduresIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_procedures_Description",
+                table: "procedures",
+                column: "Description",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_procedures_Description",
+                table: "procedures");
+        }
+    }
+}
