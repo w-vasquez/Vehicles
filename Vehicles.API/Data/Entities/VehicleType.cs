a9d025b578caef28dc3680448d4e19b0bc5a9b3c
Wilfredo Vásquez <w_vasquez@outlook.com>
2021-09-03 21:35:27 -0600

    Add vehicle type

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+	public class VehicleType
+	{
+		public int Id{ get; set; }
+
+		[Display(Name = "Tipo de vehículo")]
+		[MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres.")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string Description { get; set; }
+	}
+}
---
+++ Vehicles.API/Migrations/20210904032437_AddTableVehicleTypeWithoutErrors.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class AddTableVehicleTypeWithoutErrors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VehcleTypes",
+                table: "VehcleTypes");
+
+            migrationBuilder.RenameTable(
+                name: "VehcleTypes",
+                newName: "VehicleTypes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VehcleTypes_Description",
+                table: "VehicleTypes",
+                newName: "IX_VehicleTypes_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VehicleTypes",
+                table: "VehicleTypes",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VehicleTypes",
+                table: "VehicleTypes");
+
+            migrationBuilder.RenameTable(
+                name: "VehicleTypes",
+                newName: "VehcleTypes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VehicleTypes_Description",
+                table: "VehcleTypes",
+                newName: "IX_VehcleTypes_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VehcleTypes",
+                table: "VehcleTypes",
+                column: "Id");
+        }
+    }
+}
